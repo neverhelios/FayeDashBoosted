58697047ae85062f4756fd106b750d90e911048d
elias-bina <elias.bina58@gmail.com>
2026-05-08 12:17:58 +0200

    Add sound effect modification

--- Core.cs
+++ Core.cs
@@ -1,15 +1,80 @@
-﻿using MelonLoader;
+﻿using HarmonyLib;
+using MelonLoader;
+using System.Diagnostics;
+using UnityEngine;
 
-[assembly: MelonInfo(typeof(FayeDashBoosted.Core), "FayeDashBoosted", "1.0.0", "Elias", null)]
+[assembly: MelonInfo(typeof(FayeDashBoosted.Core), "FayeDashBoosted", "1.0.0", "NeverHeliOS", null)]
 [assembly: MelonGame("FiveHouses", "Deathbulge")]
+
 
 namespace FayeDashBoosted
 {
+    
     public class Core : MelonMod
     {
+
         public override void OnInitializeMelon()
         {
-            LoggerInstance.Msg("Initialized.");
+            LoggerInstance.Msg("Mad Dash Booster Loaded");
+        }
+    }
+
+    [HarmonyPatch(typeof(Field.FieldAbilities), "ApplyMadDash")]
+    class FayeDashPatch
+    {
+        private static int MadDashSoundIndex = 0;
+
+        public static void DrawCurrentSoundIndex()
+        {
+            GUI.Label(new Rect(30, 20, 300, 100), "<color=#c0c0c0ff><size=40>Sound index: " + MadDashSoundIndex + " </size></color>");
+        }
+
+        static void Prefix(out Stopwatch __state)
+        {
+            switch (MadDashSoundIndex)
+            {
+                case 0:
+                    CommonObjects.GetFMODSoundEvents().PlayEvent(CommonObjects.GetShopsMenuUI().menuAudio.confirmSound);
+                    break;
+                case 1:
+                    CommonObjects.GetFMODSoundEvents().PlayEvent(CommonObjects.GetShopsMenuUI().menuAudio.cancelSound);
+                    break;
+                case 2:
+                    CommonObjects.GetFMODSoundEvents().PlayEvent(CommonObjects.GetShopsMenuUI().menuAudio.menuSound);
+                    break;
+                case 3:
+                    CommonObjects.GetFMODSoundEvents().PlayEvent(CommonObjects.GetShopsMenuUI().menuAudio.merchUseSound);
+                    break;
+                case 4:
+                    CommonObjects.GetFMODSoundEvents().PlayEvent(CommonObjects.GetShopsMenuUI().menuAudio.beatBoostSound);
+                    break;
+                case 5:
+                    CommonObjects.GetFMODSoundEvents().PlayEvent(CommonObjects.GetShopsMenuUI().menuAudio.purchaseSound);
+                    break;
+                case 6:
+                    CommonObjects.GetFMODSoundEvents().PlayEvent(CommonObjects.GetShopsMenuUI().menuAudio.failSound);
+                    break;
+                case 7:
+                    CommonObjects.GetFMODSoundEvents().PlayEvent(CommonObjects.GetShopsMenuUI().menuAudio.equipSound);
+                    break;
+
+            }
+            
+            MelonEvents.OnGUI.Subscribe(DrawCurrentSoundIndex, 100);
+            
+            MadDashSoundIndex++;
+            if (MadDashSoundIndex > 7)
+                MadDashSoundIndex = 0;
+            //.menuAudio.beatBoostSound);
+            Melon<Core>.Logger.Msg("Ca se mad le dash");
+            __state = new Stopwatch(); // assign your own state
+            __state.Start();
+        }
+
+        static void Postfix(Stopwatch __state)
+        {
+            __state.Stop();
+            Melon<Core>.Logger.Msg(__state.Elapsed.ToString());
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # FayeDashBoosted
+My small first mod on deathbulge
+Grants invulerability to the Faye's dash and changes its sound effect
