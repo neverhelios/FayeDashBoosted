bd2c25bc64eb349edcfc42f861978b82a803aebc
elias-bina <elias.bina58@gmail.com>
2026-05-08 11:32:24 +0200

    Add project files.

---
+++ Core.cs
@@ -0,0 +1,15 @@
+﻿using MelonLoader;
+
+[assembly: MelonInfo(typeof(FayeDashBoosted.Core), "FayeDashBoosted", "1.0.0", "Elias", null)]
+[assembly: MelonGame("FiveHouses", "Deathbulge")]
+
+namespace FayeDashBoosted
+{
+    public class Core : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Initialized.");
+        }
+    }
+}
